6332da1ef2bc8d8ac632ec8273ab067b1f96c5fd
K.H.Madhusanka <madhusanka.samaranayake0215@gmail.com>
2025-02-20 11:30:02 +0530

    Add files via upload

---
+++ Data type 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float hours,payment,price;
+    printf("hours");
+    scanf("%f",payment);
+    price=hours*payment;
+    printf("price=%f",price);
+  return 0;
+}
