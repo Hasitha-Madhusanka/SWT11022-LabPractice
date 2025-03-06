1ca017a6b9b45c571a3a29aac39c6f6c21747a0b
K.H.Madhusanka <madhusanka.samaranayake0215@gmail.com>
2025-03-06 12:20:58 +0530

    Add files via upload

---
+++ Lab04 task01-111.c
@@ -0,0 +1,18 @@
+#Include<stdio.h>
+int main() {
+     int a=10,b=20,c=15;
+     int largest;
+     if(0a>b && a>c) {
+        largest=a;
+     } else if (b>a && b>c) {
+         largest=b;
+     } else {
+         largest=c;
+}
+printf("The largest number is: ");
+(a>b && a>c) ? printf("%d\n",a):(b.a && b>c)?print("%d\n",b):
+printf("%d\n",c):
+float average=(a+b+c)/3.0;
+printf("The average of the three numbers is: %.2f\n",average);
+return 0;
+]
